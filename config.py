016f90207505d83b92deb7282a2b2dd53c66e07e
Brav0 <68178115+jishnu10@users.noreply.github.com>
2022-08-30 13:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,8 @@ class Config(object):
       API_ID = int(os.environ.get("API_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       OWNER_ID = int(os.environ.get("OWNER_ID"))
+      LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+      AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+      DB_URL = os.environ.get("DB_URL", "")
+      DB_NAME = os.environ.get("DB_NAME", "")
 
